122818ffe27fb4a8892374d29b001078c8349cc1
Parth Arya <partharya@Parths-MacBook-Pro.local>
2020-04-25 18:47:37 -0400

    Deleting Stuff

--- form.js
+++
@@ -1,9 +0,0 @@
-$('form').submit(function(event){
-    event.preventDefault();
-
-    var form = $('form').serializeArray();
-
-    console.log(form.split('&'));
-
-    $('.results').append(form);
-});
