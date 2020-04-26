48d6bc7f3a360963520715365ca01962ac1b8534
YuanxunTao <yuanxuntao@umass.edu>
2020-04-26 19:30:28 -0400

    rename

--- pathfinder.py
+++ pathfinder.py
@@ -17,10 +17,15 @@ def all_path_to_json(all_path):
 fake_location0 = Location(0, "location 0", 0.0005, 0.0005, 5)
 fake_location1 = Location(1, "location 1", 0.001, 0.001, 10)
 fake_location2 = Location(2, "location 2", 0.0015, 0.0015, 15)
-fake_locations = [fake_location0, fake_location1, fake_location2]
+fake_location3 = Location(3, "location 3", 0.002, 0.002, 20)
+fake_locations = [fake_location0, fake_location1, fake_location2, fake_location3]
+fake_locations0 = [fake_location0, fake_location1]
+fake_locations1 = [fake_location0, fake_location3]
+fake_locations2 = [fake_location1, fake_location2]
+fake_locations3 = [fake_location3, fake_location2]
 
 locations = fake_locations
-streets = [Street(0, "street 0", fake_locations)]
+streets = [Street(0, "street 0", fake_locations0), Street(1, "street 1", fake_locations1), Street(2, "street 2", fake_locations2), Street(3, "street 3", fake_locations3)]
 start = fake_locations[0]
 end = fake_locations[2]
 shortest_distance = 0
@@ -30,9 +35,9 @@ shortest_path = []
 graph = Graph(locations, streets)
 graph.initialization()
 shortest_path, shortest_distance = graph.short_path(start, end)
-distance_limit = shortest_distance
+distance_limit = shortest_distance * 3
 
-graph.bfs(start, end, shortest_distance)
+graph.bfs(start, end, distance_limit)
 
 json_output = {
   "start_point": start.to_json(),
